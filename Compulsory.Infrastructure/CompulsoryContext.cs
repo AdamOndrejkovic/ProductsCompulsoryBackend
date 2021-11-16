421a79559e0f988ee5b30237b83d80502f3716ab
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-11-16 14:25:43 +0100

    Db init

--- Compulsory.Infrastructure/CompulsoryContext.cs
+++ Compulsory.Infrastructure/CompulsoryContext.cs
@@ -3,10 +3,26 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Compulsory.Infrastructure
 {
-    public class CompulsoryContext: DbContext
+    public class CompulsoryContext : DbContext
     {
-        public CompulsoryContext(DbContextOptions<CompulsoryContext> options): base(options){}
-        
+        public CompulsoryContext(DbContextOptions<CompulsoryContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            for (int i = 1; i < 30; i++)
+            {
+                modelBuilder.Entity<ProductEntity>()
+                    .HasData(new ProductEntity()
+                    {
+                        Id = i,
+                        Name = "Lego brick" + i,
+                        Description = "Some description",
+                        Price = 1.5 * i
+                    });
+            }
+        }
 
         public virtual DbSet<ProductEntity> Products { get; set; }
         public virtual DbSet<UserEntity> Users { get; set; }
